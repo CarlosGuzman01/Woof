40bce29b4f539af0daca8fda7be58a23ec337bdd
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-05 23:55:29 -0500

    added expand more button

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.woof
 
+import android.hardware.camera2.params.InputConfiguration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -30,14 +31,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -98,6 +107,8 @@ fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
+    
+    var expanded by remember { mutableStateOf(false)}
 
     Card(modifier = modifier){
         Row(
@@ -107,10 +118,33 @@ fun DogItem(
         ) {
             DogIcon(dog.imageResourceId)
             DogInformation(dog.name, dog.age)
+            DogItemButton(expanded = expanded, onClick = { /*TODO*/ })
         }
 
     }
 
+}
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean, 
+    onClick: () -> Unit, 
+    modifier: Modifier = Modifier
+    
+){
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ExpandMore,
+            contentDescription = stringResource(id = R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+
+
+    }
+    
 }
 
 /**
